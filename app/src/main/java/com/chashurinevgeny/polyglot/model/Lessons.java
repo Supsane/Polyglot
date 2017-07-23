559109ba50cbfd3b8e18460ff8159612e7c9e674
Supsane <echashurin@gmail.com>
2017-07-23 21:58:34 +0300

    change decor RecyclerView

--- app/src/main/java/com/chashurinevgeny/polyglot/model/Lessons.java
+++ app/src/main/java/com/chashurinevgeny/polyglot/model/Lessons.java
@@ -28,27 +28,26 @@ public class Lessons {
         public String getDescriptionLesson() {
             return descriptionLesson;
         }
-
     }
 
     private Lessons() {
         lessonExample = new ArrayList<>(16);
-        lessonExample.add(new LessonExample("Урок №1", "Базовая таблица глагола. Утверждени, отрицания, вопосы. Личные местоимения."));
-        lessonExample.add(new LessonExample("Урок №2", "Базовая таблица глагола. Утверждени, отрицания, вопосы. Личные местоимения."));
-        lessonExample.add(new LessonExample("Урок №3", "Базовая таблица глагола. Утверждени, отрицания, вопосы. Личные местоимения."));
-        lessonExample.add(new LessonExample("Урок №4", "Базовая таблица глагола. Утверждени, отрицания, вопосы. Личные местоимения."));
-        lessonExample.add(new LessonExample("Урок №5", "Базовая таблица глагола. Утверждени, отрицания, вопосы. Личные местоимения."));
-        lessonExample.add(new LessonExample("Урок №6", "Базовая таблица глагола. Утверждени, отрицания, вопосы. Личные местоимения."));
-        lessonExample.add(new LessonExample("Урок №7", "Базовая таблица глагола. Утверждени, отрицания, вопосы. Личные местоимения."));
-        lessonExample.add(new LessonExample("Урок №8", "Базовая таблица глагола. Утверждени, отрицания, вопосы. Личные местоимения."));
-        lessonExample.add(new LessonExample("Урок №9", "Базовая таблица глагола. Утверждени, отрицания, вопосы. Личные местоимения."));
-        lessonExample.add(new LessonExample("Урок №10", "Базовая таблица глагола. Утверждени, отрицания, вопосы. Личные местоимения."));
-        lessonExample.add(new LessonExample("Урок №11", "Базовая таблица глагола. Утверждени, отрицания, вопосы. Личные местоимения."));
-        lessonExample.add(new LessonExample("Урок №12", "Базовая таблица глагола. Утверждени, отрицания, вопосы. Личные местоимения."));
-        lessonExample.add(new LessonExample("Урок №13", "Базовая таблица глагола. Утверждени, отрицания, вопосы. Личные местоимения."));
-        lessonExample.add(new LessonExample("Урок №14", "Базовая таблица глагола. Утверждени, отрицания, вопосы. Личные местоимения."));
-        lessonExample.add(new LessonExample("Урок №15", "Базовая таблица глагола. Утверждени, отрицания, вопосы. Личные местоимения."));
-        lessonExample.add(new LessonExample("Урок №16", "Базовая таблица глагола. Утверждени, отрицания, вопосы. Личные местоимения."));
+        lessonExample.add(new LessonExample("Урок №1", "Базовая таблица глагола. Утверждения, отрицания, вопросы. Личные местоимения"));
+        lessonExample.add(new LessonExample("Урок №2", "Как знать тысячи слов. Местоимения, вопросительные слова, предлоги to, in, from"));
+        lessonExample.add(new LessonExample("Урок №3", "Глагол to be. Действия в процессе. Притяжательные местоимения"));
+        lessonExample.add(new LessonExample("Урок №4", "Рассказ о себе. Разговорные фразы приветствия и прощания"));
+        lessonExample.add(new LessonExample("Урок №5", "Прилагательные. Степени сравнения. Параметры времени"));
+        lessonExample.add(new LessonExample("Урок №6", "Much/many. Неопределенные местоимения. Параметры времени"));
+        lessonExample.add(new LessonExample("Урок №7", "Предложения с Let's. Новые глаголы и фразы"));
+        lessonExample.add(new LessonExample("Урок №8", "Система предлогов. Использование глаголов с предлогами"));
+        lessonExample.add(new LessonExample("Урок №9", "Новые глаголы. Возвратные местоимения. Частные слова"));
+        lessonExample.add(new LessonExample("Урок №10", "Новые слова и словосочетания. Глаголы: support, learn, cook, reserve, fly"));
+        lessonExample.add(new LessonExample("Урок №11", "Ключи: факт, процесс, результат. Чувственные состояния. О погоде"));
+        lessonExample.add(new LessonExample("Урок №12", "Разговорные фразы о путешествиях. Порядковые числительные"));
+        lessonExample.add(new LessonExample("Урок №13", "Союз \"чтобы\". Призыв к действию. Предложения с can и should. Тренировка новых глаголов"));
+        lessonExample.add(new LessonExample("Урок №14", "Фразы по телефону. Условные предложения. Вопросы к подлежащему"));
+        lessonExample.add(new LessonExample("Урок №15", "Новые глаголы, слова и фразы"));
+        lessonExample.add(new LessonExample("Урок №16", "Страдательный залог. Новые слова и фразы"));
     }
 
     static List<LessonExample> getLessonExample() {
