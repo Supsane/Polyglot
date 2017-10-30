9e060684fd4dedb776f7a8b6c9466f1c135fb147
Supsane <echashurin@gmail.com>
2017-10-30 04:49:37 +0300

    add DB

--- app/src/main/java/com/chashurinevgeny/polyglot/DataBase/DataBaseHelper.java
+++ app/src/main/java/com/chashurinevgeny/polyglot/DataBase/DataBaseHelper.java
@@ -1,8 +1,103 @@
 package com.chashurinevgeny.polyglot.DataBase;
 
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
  * Created by Chashurin Evgeny on 30.10.2017.
  */
 
-public class DataBaseHelper {
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    private static String DB_NAME = "polyglot.db";
+    private static String DB_PATH = "";
+    private static final int DB_VERSION = 1;
+
+    private SQLiteDatabase database;
+    private final Context context;
+    private boolean needUpdate = false;
+
+    public DataBaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+        if (android.os.Build.VERSION.SDK_INT >= 17)
+            DB_PATH = context.getApplicationInfo().dataDir + "/databases/";
+        else
+            DB_PATH = "/data/data/" + context.getPackageName() + "/databases/";
+        this.context = context;
+
+        importDatabase();
+
+        this.getReadableDatabase();
+    }
+
+    private boolean checkDatabase() {
+        File dbFile = new File(DB_PATH + DB_NAME);
+        return dbFile.exists();
+    }
+
+    private void importDatabase() {
+        if (!checkDatabase()) {
+            this.getReadableDatabase();
+            this.close();
+            try {
+                copyDBFile();
+            } catch (IOException iOException) {
+                throw new Error("ErrorCopyingDataBase");
+            }
+        }
+    }
+
+    public void updateDatabase() {
+        if (needUpdate) {
+            File dbFile = new File(DB_PATH + DB_NAME);
+            if (dbFile.exists()) dbFile.delete();
+
+            importDatabase();
+
+            needUpdate = false;
+        }
+    }
+
+    private void copyDBFile() throws IOException {
+        InputStream inputStream = context.getAssets().open(DB_NAME);
+        OutputStream outputStream = new FileOutputStream(DB_PATH + DB_NAME);
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inputStream.read(buffer)) > 0) outputStream.write(buffer, 0, length);
+        outputStream.flush();
+        outputStream.close();
+        inputStream.close();
+    }
+
+    public boolean openDatabase() throws SQLException {
+        database = SQLiteDatabase.openDatabase(DB_PATH + DB_NAME, null, SQLiteDatabase.CREATE_IF_NECESSARY);
+        return database !=null;
+    }
+
+    @Override
+    public synchronized void close() {
+        if (database != null)
+            database.close();
+        super.close();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (newVersion > oldVersion) {
+            needUpdate = true;
+        }
+    }
 }
